6487323ca8f6ddc2c22810e2c01325809d7e2808
Zebaro <denok100100@gmail.com>
2025-07-18 19:15:50 +0200

    Modify runner.py - Add run function

--- runner.py
+++ runner.py
@@ -1,6 +1,12 @@
+from telegram_bot import TelegramBot
+
+
 class Runner:
+    @staticmethod
     def run(self):
-        pass
+        telegram_bot = TelegramBot()
+        telegram_bot.setup_handlers()
+        telegram_bot.start_polling()
 
 
 if __name__ == '__main__':
